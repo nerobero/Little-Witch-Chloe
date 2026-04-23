ea988913a8775bee1c59fbe5dc66aea5b2cc81ca
7heIVIaze <kimdave205@gmail.com>
2026-04-23 16:54:48 +0900

    Created enemy base classes.
    focus on working to Controllerbase

---
+++ Assets/Script/Enemy/EnemyControllerBase.cs
@@ -0,0 +1,180 @@
+using UnityEngine;
+
+public class EnemyControllerBase : MonoBehaviour
+{
+    #region ReferenceClasses
+    // Reference to input-event binds
+
+    // Handler for player's movement
+    private EnemyMovement enemyMove;
+
+//     // Handler for player's attack system
+//     private PlayerAttack _playerAttack;
+    #endregion
+
+//     [Header("Attack - Input hold duration")]
+//     [SerializeField] private float chargeThreshold = 1.5f; // seconds to trigger a charged attack
+//     [SerializeField] private float maxChargeTime = 3f;
+//     private float _attackPressTime = -1f;
+
+//     // Used Time.time instead of Time.deltaTime because
+//     // the charge logic is directly related to the actual timestamp,
+//     // which is not something that accumlates PER frame
+//     public float CurrentChargeRatio =>
+//         _attackPressTime < 0f ? 0f : Mathf.Clamp01((Time.time - _attackPressTime / maxChargeTime));
+
+
+//     // Jump => Flying transition related variables:
+//     [Header("Jump => Flying transition")]
+//     [SerializeField] private float flyingThreshold = 1.5f;
+//     public bool IsFlying => _isFlying;
+//     private bool _isFlying = false;
+//     private float _jumpPressTime = -1f;
+
+
+
+//     #region Setup
+//     private void Awake()
+//     {
+//         // assigning the instance:
+//         _instance = this;
+//         // create PlayerInput:
+//         _inputContext = new PlayerInput();
+
+//         // Caching once, never having to re-fetch again:
+//         _playerMove = GetComponent<PlayerMovement>();
+//         _playerAttack = GetComponent<PlayerAttack>();
+//         // _animator = GetComponent<PlayerAnimator>();
+//     }
+
+//     private void OnEnable()
+//     {
+//         //call BaseInputAction.AddCallbacks(this)
+//         _inputContext.BaseInputAction.AddCallbacks(this);
+
+//         // enable the input action binding
+//         _inputContext.BaseInputAction.Enable();
+
+//     }
+
+//     private void OnDisable()
+//     {
+//         //disable the input action binding
+//         _inputContext.BaseInputAction.Disable();
+
+//         _inputContext.BaseInputAction.RemoveCallbacks(this);
+//     }
+
+//     private void OnDestroy()
+//     {
+//         // when the controller is destroyed, we also dispose of the inputcontext
+//         // preventing any leftover memory references at runtime
+//         _inputContext.Dispose();
+//     }
+//     #endregion
+
+//     #region InputCallbacks
+
+//     // Passes the float value caught from the input callback context
+//     // directly into the PlayerMovement move direction
+//     // public void OnMoveLeftRight(InputAction.CallbackContext context)
+//     //     => _playerMove.SetMoveDirection(context.ReadValue<float>());
+
+
+//     // Calls PlayerMovement.Jump if the input has been performed
+//     public void OnJump(InputAction.CallbackContext context)
+//     {
+//         // if (context.performed) _playerMove.Jump();
+
+//         /*
+//         2026.04.16: Note from Angela -
+//         Because of the newly added feature 'flying' which shares the same key as jump, 
+//         I added logic gates that determines whether to jump or to fly depending on the key hold time.
+//         */
+//         if (context.started)
+//         {
+//             // keeping a snapshot of the time at which the jump was first processed:
+//             _jumpPressTime = Time.time;
+//             _playerMove.Jump(); // immediate jump on press
+//         }
+//         else if (context.canceled)
+//         {
+//             // when cancelled, resetting the snapshot to negative (i.e., invalid) value:
+//             _jumpPressTime = -1f;
+
+//             // if the player was flying:
+//             if (_isFlying)
+//             {
+//                 _isFlying = false;
+//                 _playerMove.StopFlying();
+//             }
+//         }
+
+//     }
+
+//     private void Update()
+//     {
+//         // if there is a valid time snapshot for jump start and 
+//         // the player is not already flying:
+//         if (_jumpPressTime >= 0f && !_isFlying)
+//         {
+//             // elapsed duration of the hold = current time - the jump start time snapshot
+//             float heldFor = Time.time - _jumpPressTime;
+//             if (heldFor >= flyingThreshold) // state change
+//             {
+//                 _isFlying = true;
+//                 _playerMove.StartFlying(); // always called before FlyTick()
+//             }
+//         }
+//     }
+
+//     private void FixedUpdate()
+//     {
+//         if (_isFlying)
+//             _playerMove.FlyTick(); // force applied here, every frame after Update()
+//     }
+
+//     public void OnAimAttack(InputAction.CallbackContext context) { }
+
+//     public void OnAttack(InputAction.CallbackContext context)
+//     {
+//         /*
+//         2026.04.15: 
+//         Combined input context catching for normal and charged attacks into one input callback
+//         function instead of two. 
+//         'Hold' interaction in the input action is for actions that requires the player to commit
+//         to a full hold, e.g., a context menu long-press.  
+//         Thus, we keep track of the duration of which the player holds the input key down and call different
+//         functions depending on that tracked duration.
+//         */
+//         if (context.started)
+//         {
+//             _attackPressTime = Time.time; // button down — start tracking
+//         }
+//         else if (context.canceled)
+//         {
+//             if (_attackPressTime < 0f) return;
+
+//             float heldFor = Time.time - _attackPressTime;
+//             _attackPressTime = -1f;
+
+//             if (heldFor >= chargeThreshold)
+//             {
+//                 float chargeRatio = Mathf.Clamp01(heldFor / maxChargeTime);
+//                 _playerAttack.FireCharged(chargeRatio); // 0.0 = min charge, 1.0 = full
+//             }
+//             else
+//             {
+//                 _playerAttack.FireNormal();
+//             }
+//         }
+
+//     }
+
+//     public void OnBlink(InputAction.CallbackContext context)
+//     {
+//         if (context.performed) _playerMove.BlinkToOtherPlatform();
+//     }
+
+//     #endregion
+}
---
+++ Assets/Script/Enemy/EnemyMovement.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+
+/// <summary>
+/// Processes the movement and the physics of the enemy character
+/// given the vector/axis values from the enemy controller.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour
+{
+     // These values are exposed states for others to read:
+    public bool IsGrounded { get; private set; }
+    public float MoveDir { get; private set; }
+
+    [Header("Movement values")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float flyForce;
+
+    // Physics body for 2D object
+    private Rigidbody2D _rb;
+
+    // @TODO: Add a serialized private/public PlayerAnimControl class reference here
+
+    private void Awake()
+    {
+        // makes sure that we auto-get the reference for the rigidbody at runtime:
+        _rb = GetComponent<Rigidbody2D>();
+        
+        //
+        Invoke("Think", 5);
+    }
+
+    // Physics is based on time (in seconds), thus we should use FixedUpdate
+    // which is not called per-tick.
+    private void FixedUpdate()
+    {
+        // Move
+        _rb.linearVelocity = new Vector2(MoveDir * speed, _rb.linearVelocity.y); 
+
+        // Platform check
+        Vector2 frontVec = new Vector2(_rb.position.x + 0.5f * MoveDir * speed,
+        _rb.position.y);
+
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1,
+        LayerMask.GetMask("Platform"));
+
+        // If the next position is cliff, then change its direction
+        if(rayHit.collider == null)
+        {
+            MoveDir *= -1;
+
+            // Cancel all invoke function
+            CancelInvoke();
+
+            // Think next behavior after 3 seconds.
+            Invoke("Think", 3);
+        }
+    }
+
+    // To change the behavior
+    protected void Think()
+    {
+        MoveDir = Random.Range(-1, 2);
+
+        float nextThinkTime = Random.Range(2f, 5f);
+
+        Invoke("Think", nextThinkTime);
+    }
+
+    public void SetMoveDirection(float direction)
+    {
+        MoveDir = direction;
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded)
+        {
+            _rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+
+        // BONUS logic here if needed:
+    }
+
+    public void StartFlying()
+    {
+        // if this function is called when the character is NOT set to fly,
+        // then return.
+        if (!PlayerController.Instance.IsFlying) return;
+
+        // flying physics logic here
+        _rb.gravityScale = 0.75f; // reducing the gravity by a quarter for more floaty feel 
+
+        // TODO: add the start flying animation state change here:
+    }
+
+    public void StopFlying()
+    {
+        // if this function is called when the character is STILL set to fly,
+        // then return.
+        if (PlayerController.Instance.IsFlying) return;
+
+        // stop flying physics logic here
+        _rb.gravityScale = 1f; // restoring the default gravity value
+
+        // TODO: add the stop flying animation state change here:
+    }
+
+    public void FlyTick()
+    {
+        _rb.AddForce(Vector2.up * flyForce, ForceMode2D.Force);
+    }
+
+    public void BlinkToOtherPlatform()
+    {
+        /*
+        'Blinking' is basically the term for teleporting between the foreground and background platforms.
+        We may need to have our own calculation system for determining where on the platform Chloe should
+        teleport to. 
+        */
+    }
+}
