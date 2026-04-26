d0085ba1596f3d3ae0e6f21bb43201e651a482c2
nerobero <rhee.ji@northeastern.edu>
2026-04-26 16:33:27 +0900

    added baseline logic for blinking
    TODO: need to change the origin vector to something else for distance effect

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -16,9 +17,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
     [SerializeField] private float flyForce;
+    [SerializeField] private LayerMask bgLayer;
+    [SerializeField] private LayerMask fgLayer;
+
+    [Header("Debugging, Testing toggler")]
+    [SerializeField] private bool Is3D;
 
     // Physics body for 2D object
     private Rigidbody2D _rb;
+    private bool _isBackground = false;
 
     // @TODO: Add a serialized private/public PlayerAnimControl class reference here
 
@@ -93,5 +100,27 @@ public class PlayerMovement : MonoBehaviour
         We may need to have our own calculation system for determining where on the platform Chloe should
         teleport to. 
         */
+        if (Is3D)
+        {
+
+        }
+        else
+        {
+            //1. finding if there is any teleportable platform within the given radius 
+            LayerMask layerParam = _isBackground ? bgLayer : fgLayer;
+            if (Physics2D.OverlapCircle(transform.position, 15.0f, layerParam) == null)
+                return;
+
+            //2. find the surface to get teleport to:
+            float camHalfHeight = Camera.main.orthographicSize;
+            // TODO: may need to change this to another position vector
+            Vector2 camOrigin = (Vector2)Camera.main.transform.position;
+            RaycastHit2D hit2D = Physics2D.Raycast(camOrigin + Vector2.up * camHalfHeight,
+                    Vector2.down, camHalfHeight * 2f, layerParam);
+            if (hit2D.collider == null) return;
+
+            //3. reposition the player character:
+            _rb.position = new Vector2(_rb.position.x, hit2D.point.y);
+        }
     }
 }
