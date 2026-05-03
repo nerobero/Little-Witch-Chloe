fe3379f1cf6623216e2fc466c58e1e70af02f0a3
nerobero <rhee.ji@northeastern.edu>
2026-05-03 21:57:13 +0900

    created a base character anim controller

---
+++ Assets/Script/Characters/BaseCharacterAnimController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/// <summary>
+/// Base controller for character animation.
+/// All state change logic for the character's animation is handled here
+/// for the simplicity of debugging and better readability.
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class BaseCharacterAnimController : MonoBehaviour
+{
+    protected static readonly int IdleHash = Animator.StringToHash("IdleHash");
+    protected static readonly int WalkingHash = Animator.StringToHash("IsWalking");
+    protected static readonly int BlinkingHash = Animator.StringToHash("IsBlinking");
+
+    protected Animator _animator;
+
+    public bool IsFacingRight = true;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Flips the owner of this component on the x-axis.
+    /// </summary>
+    public void FlipCharacter(float moveDir)
+    {
+        // we only process the character to flip when the 
+        if (!IsFacingRight && moveDir > 0f || IsFacingRight && moveDir < 0f)
+        {
+            IsFacingRight = !IsFacingRight;
+            Vector2 localScale2D = transform.localScale;
+            localScale2D.x *= -1f;
+            transform.localScale = localScale2D;
+        }
+    }
+
+    public void SetToWalk(bool isWalking)
+    {
+        _animator.SetBool(WalkingHash, isWalking);
+    }
+}
--- Assets/Script/Characters/Player/PlayerAnimController.cs
+++ Assets/Script/Characters/Player/PlayerAnimController.cs
@@ -6,40 +6,12 @@ using UnityEngine;
 /// for the simplicity of debugging and better readability.
 /// </summary>
 [RequireComponent(typeof(Animator))]
-public class PlayerAnimController : MonoBehaviour
+public class PlayerAnimController : BaseCharacterAnimController
 {
-    private static readonly int IdleHash = Animator.StringToHash("IdleHash");
-    private static readonly int WalkingHash = Animator.StringToHash("IsWalking");
     private static readonly int IsFlyingHash = Animator.StringToHash("IsFlying");
     private static readonly int FlyTickHash = Animator.StringToHash("FlyTick");
 
-    private Animator _animator;
     public bool _isFacingRight = true;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
-    /// <summary>
-    /// Flips the owner of this component on the x-axis.
-    /// </summary>
-    public void FlipCharacter(float moveDir)
-    {
-        // we only process the character to flip when the 
-        if (!_isFacingRight && moveDir > 0f || _isFacingRight && moveDir < 0f)
-        {
-            _isFacingRight = !_isFacingRight;
-            Vector2 localScale2D = transform.localScale;
-            localScale2D.x *= -1f;
-            transform.localScale = localScale2D;
-        }
-    }
-
-    public void SetToWalk(bool isWalking)
-    {
-        _animator.SetBool(WalkingHash, isWalking);
-    }
 
     public void SetToStartFlying()
     {
