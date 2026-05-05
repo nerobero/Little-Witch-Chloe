7b1dd23badc90576b0e742d41d9e9f4dd7ee761c
nerobero <rhee.ji@northeastern.edu>
2026-05-05 14:48:22 +0900

    created regenerative item classes (HP, stamina)

---
+++ Assets/Script/System/Interaction/RegenHPItem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Types;
+
+/// <summary>
+/// 
+/// </summary>
+public class RegenHPItem : RegenItemBase
+{
+    protected override bool OnInteractHelper(Collider2D other)
+    {
+        throw new System.NotImplementedException();
+    }
+}
