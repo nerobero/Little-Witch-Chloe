c90a805586081a26668a39b981e95e8ecf0e86b3
shio <shiorithompson@gmail.com>
2026-05-11 03:26:39 -0400

    Sound Manager added

---
+++ Assets/Script/System/Sound/ForestMusicState1.cs
@@ -0,0 +1,33 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+public class ForestMusicState1 : MonoBehaviour
+{
+    private SoundManager soundManager;
+    private float forestLevelValue = 1f;
+
+    private void Start()
+    {
+        soundManager = FindFirstObjectByType<SoundManager>();
+        if (soundManager == null)
+            Debug.LogError("SoundManager not found in scene!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        print("Hit: " + collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            if (soundManager.GetGlobalValue() != 1f)
+            {
+                soundManager.SetGlobalValue(1f);
+            }
+        }
+    }
+}
+
+
+
+
