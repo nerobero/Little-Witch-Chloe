b2994ab457f06f86b1a258199a9ea2c7c4c5a24d
7heIVIaze <kimdave205@gmail.com>
2026-07-16 15:56:08 +0900

    Created Blinded for testing. there is no actual effect now

---
+++ Assets/Script/System/Combat/BuffSystem/BlindedEffect.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Types;
+using Data;
+
+[System.Serializable]
+public class BlindedEffect : StatusEffect
+{
+    public BlindedEffect(StatManager owner, float duration)
+        : base(owner, EStatusEffectType.Blind, EStatusEffectCategory.CrowdControl, null, duration)
+    {}
+
+
+    public override void Apply(StatManager target)
+    {
+        Debug.Log("Blind Applied");
+        target.AddCrowdControl(ECrowdControlType.Blinded);
+    }
+
+    public override void Remove(StatManager target)
+    {
+        target.RemoveCrowdControl(ECrowdControlType.Blinded);
+    }
+}
