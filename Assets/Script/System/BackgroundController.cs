14fff4c0cac388f2a9fabbbc34e335bb50650b1e
meeecoh <my.coworker.art@gmail.com>
2026-07-25 18:51:19 -0700

    Update BackgroundController.cs

--- Assets/Script/System/BackgroundController.cs
+++ Assets/Script/System/BackgroundController.cs
@@ -4,31 +4,43 @@ using Types;
 
 public class BackgroundController : MonoBehaviour 
 {
-    private float startPos, length;
+    private Vector2 startPos;
     public Camera cam;
-    public float parallexEffect; // 0.15 is good for the tree backgrounds.
+    public GameObject parallaxPoint;
+    public float parallexEffectX; // 0.15 is good for the tree backgrounds.
+    public float parallexEffectY;
+    public bool enableYParallax;
 
     void Start()
     {
         cam = Camera.main;
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position;
     }
 
     void FixedUpdate()
-    {
-        float distance = cam.transform.position.x * parallexEffect;
-        float movement = cam.transform.position.x * (1 - parallexEffect);
-
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
-
-        if(movement > startPos + length)
+    {   
+        //fallback to cam pos in worldspace if parallaxPoint is not set
+        Vector2 camPos;
+        if (parallaxPoint != null)
         {
-            startPos += length;
-        }
-        else if(movement < startPos - length)
+            camPos = cam.transform.position - parallaxPoint.transform.position;
+        } else 
         {
-            startPos -= length;
+            camPos = cam.transform.position;
         }
+        
+        Vector2 distance = Vector2.Scale(camPos, new Vector2(parallexEffectX, parallexEffectY));
+
+
+        if (enableYParallax)
+        {
+            transform.position = new Vector3(startPos.x + distance.x, startPos.y + distance.y, transform.position.z);
+        }
+        else
+        {
+            // only x-parallax on
+            transform.position = new Vector3(startPos.x + distance.x, startPos.y, transform.position.z);
+        }
+
     }
 }
