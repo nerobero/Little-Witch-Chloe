854be926c12f99a0fb8679c8239d685fe125064b
7heIVIaze <kimdave205@gmail.com>
2026-08-30 21:37:44 +0900

    push missed cameracontroller logic

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -6,21 +6,45 @@ public class CameraController : MonoBehaviour
     public float smoothTime = 0.2f;
     public Vector3 offset = new Vector3(0, 1.0f, -10.0f);
     private Vector3 velocity = Vector3.zero;
+    private bool isCameraLocked = false;
+    private Camera cam;
+    private float originalSize;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        player = FindObjectOfType<PlayerController>();
+        player = FindFirstObjectByType<PlayerController>();
+        cam = GetComponent<Camera>();
+
+        originalSize = cam.orthographicSize;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if(player != null)
+        if(isCameraLocked == false)
         {
-            Vector3 playerPosition = player.transform.position + offset;
+            if(player != null)
+            {
+                Vector3 playerPosition = player.transform.position + offset;
 
-            transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, smoothTime);
+                transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, smoothTime);
+            }
         }
+    }
+
+    public void LockCamera(bool isLocked)
+    {
+        isCameraLocked = isLocked;
+    }
+
+    public void UpdateCameraSize(float size = 0)
+    {
+        cam.orthographicSize = size;
+    }
+
+    public void UpdateCameraSizeToOriginal()
+    {
+        cam.orthographicSize = originalSize;
     }
 }
