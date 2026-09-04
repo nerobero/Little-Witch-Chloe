181b8c24071a449fd0ed42a0a2b7e34fecbd5562
shio <shiorithompson@gmail.com>
2026-09-03 20:06:24 -0700

    Sign sound added

--- Assets/Script/System/Interaction/ManualInteract/MessageBoxTrigger.cs
+++ Assets/Script/System/Interaction/ManualInteract/MessageBoxTrigger.cs
@@ -6,6 +6,7 @@ public class MessageBoxTrigger : InteractableBase
     [SerializeField] private string messageDataKey = "";  
     protected override void Interact_Impl()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Dialouge");
          UIManager.Instance.Get<UIMessageBox>().ShowMessage(messageDataKey);
     }
 }
