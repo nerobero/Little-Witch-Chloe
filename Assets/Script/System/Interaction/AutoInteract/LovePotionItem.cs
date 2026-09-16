418ed85d2c4d3ad701de019ed48d0b4f998a7451
7heIVIaze <kimdave205@gmail.com>
2026-09-16 21:34:55 +0900

    Create Lovepotion item

---
+++ Assets/Script/System/Interaction/AutoInteract/LovePotionItem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LovePotionItem : ItemBase
+{
+    protected override bool OnInteract(Collider2D other)
+    {
+        if(!_canInteract) return false;
+        
+        return OnInteract_Helper(other);
+    }
+
+    protected bool OnInteract_Helper(Collider2D other)
+    {
+        if(LayerMask.LayerToName(other.gameObject.layer).Contains("Player"))
+        {
+            GameManager.Instance.OnLovePotionMade();            
+        }
+
+        return false;
+    }
+}
